fef141d742274e1e54fd695f424e94119da4ffb9
Le Van <12345levan@gmail.com>
2026-04-24 20:53:21 +0700

    Fix missing MaterialTheme import in BookImageHeader

--- BookStore/app/src/main/java/com/example/bookstore/ui/components/BookImageHeader.kt
+++ BookStore/app/src/main/java/com/example/bookstore/ui/components/BookImageHeader.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
